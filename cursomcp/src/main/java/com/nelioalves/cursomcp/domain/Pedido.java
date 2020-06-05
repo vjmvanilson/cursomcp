857eeef3220f44c91877f0b543836d2a0b6bea13
vanilson <vanilsonvjm@gmail.com>
2020-06-05 16:35:27 -0300

    calculo de produto

--- cursomcp/src/main/java/com/nelioalves/cursomcp/domain/Pedido.java
+++ cursomcp/src/main/java/com/nelioalves/cursomcp/domain/Pedido.java
@@ -58,8 +58,8 @@ public class Pedido implements Serializable {
 
 	public double getValorTotal() {
 		double soma = 0.0;
-		for (ItemPedido ip : itens) {
-			soma = soma + ip.getSubTotal();
+		for (ItemPedido itemP : itens) {
+			soma = soma + itemP.getSubTotal();
 		}
 		return soma;
 	}
